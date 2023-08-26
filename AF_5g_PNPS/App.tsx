22a6f57e77f4985d7fb1f0b107fd7f5821605584
이진형 <dlwlsgud2416@naver.com>
2023-08-26 14:36:30 +0000

    expo 해결

--- AF_5g_PNPS/App.tsx
+++ AF_5g_PNPS/App.tsx
@@ -1,20 +1,51 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import React, { useState, useEffect } from 'react';
+import { Appearance, AppearanceProvider } from 'react-native-appearance';
+import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Provider as PaperProvider } from 'react-native-paper';
 import ChatScreen from './ChatScreen';
 import HomeScreen from './HomeScreen';
 import ProfileScreen from './ProfileScreen';
 
 const Tab = createBottomTabNavigator();
 
+const CombinedDefaultTheme = {
+  ...DefaultTheme,
+  ...DefaultTheme,
+ };
+
+const CombinedDarkTheme = {
+  ...DarkTheme,
+  ...DarkTheme,
+};
+
 export default function App() {
+  const colorScheme = Appearance.getColorScheme(); // 시스템 다크 모드 설정 감지
+  const [isDarkMode, setIsDarkMode] = useState(colorScheme === 'dark');
+
+  useEffect(() => {
+    // Appearance 라이브러리를 사용하여 시스템 다크 모드 설정을 구독
+    const subscription = Appearance.addChangeListener(({ colorScheme }) => {
+      setIsDarkMode(colorScheme === 'dark');
+    });
+
+    // 구독 취소
+    return () => subscription.remove();
+  }, []);
+
+  const theme = isDarkMode ? CombinedDarkTheme : CombinedDefaultTheme;
+
   return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Chat" component={ChatScreen} />
-        <Tab.Screen name="Profile" component={ProfileScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <AppearanceProvider>
+      <PaperProvider theme={theme}>
+        <NavigationContainer theme={theme}>
+          <Tab.Navigator>
+            <Tab.Screen name="Home" component={HomeScreen} />
+            <Tab.Screen name="Chat" component={ChatScreen} />
+            <Tab.Screen name="Profile" component={ProfileScreen} />
+          </Tab.Navigator>
+        </NavigationContainer>
+      </PaperProvider>
+    </AppearanceProvider>
   );
 }
