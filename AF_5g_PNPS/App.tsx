e2d816d40399f458d233a11b11095180cd359eae
이진형 <dlwlsgud2416@naver.com>
2023-08-26 13:08:49 +0000

    프로젝트 시작

--- AF_5g_PNPS/App.tsx
+++ AF_5g_PNPS/App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>와 됐다 ㅋㅋㄹㅃㅃ ㅅㄱㄹ</Text>
       <StatusBar style="auto" />
     </View>
   );
