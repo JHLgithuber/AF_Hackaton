ea59265d80639fb504309f50332587e7e74fef5d
이진형 <dlwlsgud2416@naver.com>
2023-08-29 14:56:03 +0000

    rsa암호화 구현 테스트

--- RSA_Example/App.tsx
+++ RSA_Example/App.tsx
@@ -1,10 +1,13 @@
-import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
 
 //import 'react-native-rsa-expo';
 
+var Start_MakingKey = new Date();
+console.log("\n\n");
+console.log(Start_MakingKey)
 const RSAKey = require('react-native-rsa');
-const bits = 4056;
+const bits = 725;
 const exponent = '10001'; // must be a string. This is hex string. decimal = 65537
 const rsa = new RSAKey();
 rsa.generate(bits, exponent);
@@ -12,28 +15,28 @@ const publicKey = rsa.getPublicString(); // return json encoded string
 const privateKey = rsa.getPrivateString(); // return json encoded string
 console.log(publicKey);
 console.log(privateKey);
+var End_MakingKey = new Date();
+console.log(End_MakingKey-Start_MakingKey);
 
 //const rsa = new RSAKey();
-rsa.setPublicString(publicKey);
-const originText = "계절이 지나가는 하늘에는 가을로 가득 차 있습니다.계절이 지나가는 하늘에는 가을로 가득 차 있습니다.";
-const encrypted = rsa.encrypt(originText);
-console.log(encrypted);
-
-rsa.setPrivateString(privateKey);
-const decrypted = rsa.decrypt(encrypted); // decrypted == originText
-console.log(decrypted);
-
-
-class App extends Component {
-  render () {
-    return (
-      <View>
-        <Text>
-          Demo
-        </Text>
-      </View>
-    )
-  }
+for (i = 0; i < 3; i++) {
+	rsa.setPublicString(publicKey);
+	const originText = '계절이 지나가는 하늘에는 가을로 가득 차 있습니다.';
+	const encrypted = rsa.encrypt(originText);
+	console.log(encrypted);
+	rsa.setPrivateString(privateKey);
+	const decrypted = rsa.decrypt(encrypted); // decrypted == originText
+	console.log(decrypted);
 }
 
-export default App
+class App extends Component {
+	render() {
+		return (
+			<View>
+				<Text>Demo</Text>
+			</View>
+		);
+	}
+}
+
+export default App;
