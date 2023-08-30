658c04d974dd5506e23f7dd290c57d87b9487c8d
이진형 <dlwlsgud2416@naver.com>
2023-08-30 06:40:43 +0000

    sqlite 저장 구현

--- AF_5g_PNPS/ChatScreen.js
+++ AF_5g_PNPS/ChatScreen.js
@@ -1,31 +1,19 @@
-// ChatScreen.js
 import React, { useState, useEffect } from 'react';
 import { GiftedChat, IMessage } from 'react-native-gifted-chat';
 import * as SQLite from 'expo-sqlite';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import CryptoJS from 'crypto-js';
 
-const Chat_DB = SQLite.openDatabase('Encrypted_Chat_Data.db'); //채팅저장용 SQLite
+// 채팅 저장을 위한 SQLite 데이터베이스를 열기
+const Chat_DB = SQLite.openDatabase('Encrypted_Chat_Data.db');
 
 export default function ChatScreen() {
 	const RoomName = 'test_room';
-
-	const [messages, setMessages] = useState([
-		{
-			_id: 1,
-			text: 'Hello!',
-			createdAt: new Date(),
-			user: {
-				_id: 2,
-				name: 'User',
-			},
-		},
-	]);
+	const [messages, setMessages] = useState([]);
 
 	const fetchMessages = () => {
+		let rowArray = [];
 		Chat_DB.transaction((tx) => {
 			tx.executeSql(
-				`SELECT * FROM ${RoomName};`,
+				`SELECT * FROM ${RoomName}`,
 				[],
 				(_, { rows }) => {
 					for (let i = 0; i < rows.length; i++) {
@@ -33,156 +21,146 @@ export default function ChatScreen() {
 						const rowMessage: IMessage = {
 							_id: item.id,
 							text: item.encrypt_data,
-							createdAt: item.date,
+							createdAt: new Date(item.date),
 							user: {
 								_id: item.sender,
-								name: '시스템',
+								name: item.sender_name,
 							},
 						};
 						rowArray.push(rowMessage);
 					}
-						
-						console.log('전체 데이터 배열:', rowArray);
-				setMessages(rowArray); // 업데이트된 배열로 메시지 업데이트
-					console.log('전체 데이터 배열:', rowArray);
+					setMessages(rowArray);
 				},
 				(_, err) => {
-					console.log('데이터 조회 실패:', err);
+					console.log('Fetch Error:', err);
+					return false;
 				}
 			);
 		});
 	};
 
-	const removeMessages = async () => {
-		try {
+	const Make_new_DB = () => {
+		Chat_DB.transaction((tx) => {
+			tx.executeSql(
+				`CREATE TABLE IF NOT EXISTS ${RoomName} (id INTEGER PRIMARY KEY AUTOINCREMENT, date TEXT, sender TEXT, sender_name TEXT, receiver INTEGER, peer_key_hash TEXT, server_key_hash TEXT, encrypt_AES_Key TEXT, encrypt_data BLOB);`,
+				[],
+				(_, result) => {
+					console.log('Table Create Success:', result);
+					fetchMessages();
+				},
+				(_, err) => {
+					console.log('Table Create Error:', err);
+					return false;
+				}
+			);
+		});
+	};
+
+	const generateUniquePK = () => {
+		// 현재의 유닉스 시간을 밀리초 단위로 가져옵니다.
+		const unixTimeMillis = new Date().getTime();
+
+		// 0부터 9999까지 랜덤한 정수를 생성합니다.
+		const randomNumber = Math.floor(Math.random() * 10000);
+
+		// 두 값을 문자열로 변환하고 이어붙여 고유한 PK를 생성합니다.
+		const uniquePK = String(unixTimeMillis) + String(randomNumber);
+
+		return uniquePK;
+	};
+
+	const onSend = (newMessages = []) => {
+		setMessages((prevMessages) => GiftedChat.append(prevMessages, newMessages));
+
+		newMessages.forEach((message) => {
+			if (message.text === '/r') {
+				// 테이블 삭제
+				Chat_DB.transaction((tx) => {
+					tx.executeSql(
+						`DROP TABLE IF EXISTS ${RoomName};`,
+						[],
+						(_, result) => {
+							console.log('Table Dropped:', result);
+							// 테이블 다시 생성
+							Make_new_DB();
+						},
+						(_, err) => {
+							console.log('Drop Table Error:', err);
+							return false;
+						}
+					);
+				});
+				setMessages([]); // 화면에서 모든 메시지 제거
+				return; // 이후 처리를 중단
+			}
+			console.log(typeof parseInt(message._id, 10));
+
+			// 기존의 메시지 삽입 로직
 			Chat_DB.transaction((tx) => {
 				tx.executeSql(
-					`DROP TABLE IF EXISTS ${RoomName};`,
-					[],
+					`INSERT INTO ${RoomName} (id, date, sender,sender_name, receiver, peer_key_hash, server_key_hash, encrypt_AES_Key, encrypt_data) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+					[
+						generateUniquePK(),
+						message.createdAt.toISOString(),
+						message.user._id,
+						message.user.name,
+						null,
+						null,
+						null,
+						null,
+						message.text,
+					],
 					(_, result) => {
-						console.log('테이블 삭제 성공:', result);
+						console.log('Insert Success:', result);
 					},
 					(_, err) => {
-						console.log('테이블 삭제 실패:', err);
+						console.log('Insert Error:', err);
 						return false;
 					}
 				);
-				console.log('메시지가 삭제되었습니다.');
 			});
-		} catch (e) {
-			console.error('메시지 삭제 실패:', e);
-		}
+		});
 	};
 
-	const onSend = async (newSendingMessages: IMessage[]) => {
-		try {
-			const updatedMessages = GiftedChat.append(messages, newSendingMessages);
-			Chat_DB.transaction((tx) => {
-				tx.executeSql(
-					`INSERT INTO ${RoomName} (id, date, sender, receiver, peer_key_hash, server_key_hash, encrypt_AES_Key, encrypt_data) VALUES (?, ?, ?, ?, ?, ?, ?, ?);`,
-					[
-						newSendingMessages[0]._id.toString(),
-						newSendingMessages[0].createdAt.toISOString(),
-						newSendingMessages[0].user._id.toString(),
-						null,
-						null,
-						null,
-						null,
-						newSendingMessages[0].text.toString(),
-					],
-					(_, result) => {
-						console.log('데이터 삽입 성공:', result);
-					},
-					(_, err) => {
-						console.log('데이터 삽입 실패:', err);
-						return false; // 롤백
-					}
-				);
-			});
-			if (newSendingMessages[0].text === '/r') {
-				await removeMessages();
-			}
+	const onReceive = (newReceivingMessage: IMessage) => {
+		// 새로운 메시지 객체를 생성하고 _id에 고유한 PK를 할당
+		const updatedMessage = { ...newReceivingMessage, _id: generateUniquePK() };
+		setMessages((prevMessages) => GiftedChat.append(prevMessages, [updatedMessage]));
 
-			setMessages(updatedMessages); //화면 시현
-		} catch (e) {
-			console.error('메시지 전송 실패:', e);
-		}
-	};
-
-	const onReceive = async (newReceivingMessage: IMessage) => {
-		try {
-			const updatedMessages = GiftedChat.append(messages, newReceivingMessage);
-			Chat_DB.transaction((tx) => {
-				tx.executeSql(
-					`INSERT INTO ${RoomName} (id, date, sender, receiver, peer_key_hash, server_key_hash, encrypt_AES_Key, encrypt_data) VALUES (?, ?, ?, ?, ?, ?, ?, ?);`,
-					[
-						newReceivingMessage[0]._id.toString(),
-						newReceivingMessage[0].createdAt.toISOString(),
-						newReceivingMessage[0].user._id.toString(),
-						null,
-						null,
-						null,
-						null,
-						newReceivingMessage[0].text.toString(),
-					],
-					(_, result) => {
-						console.log('데이터 삽입 성공:', result);
-					},
-					(_, err) => {
-						console.log('데이터 삽입 실패:', err);
-						return false; // 롤백
-					}
-				);
-			});
-
-			setMessages(updatedMessages); //화면 시현
-		} catch (e) {
-			console.error('메시지 수신 실패:', e);
-		}
+		// 메시지 수신 로직
+		// 데이터베이스에 새로운 수신 메시지를 저장
+		Chat_DB.transaction((tx) => {
+			tx.executeSql(
+				`INSERT INTO ${RoomName} (id, date, sender, sender_name, receiver, peer_key_hash, server_key_hash, encrypt_AES_Key, encrypt_data) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+				[
+					generateUniquePK(),
+					newReceivingMessage.createdAt.toISOString(),
+					newReceivingMessage.user._id,
+					newReceivingMessage.user.name,
+					null,
+					null,
+					null,
+					null,
+					newReceivingMessage.text,
+				],
+				(_, result) => {
+					console.log('Receive Insert Success:', result);
+				},
+				(_, err) => {
+					console.log('Receive Insert Error:', err);
+					return false;
+				}
+			);
+		});
 	};
 
 	useEffect(() => {
-		// 테이블 생성
-		Chat_DB.transaction((tx) => {
-			console.log("이거 안하냐");
-			tx.executeSql(
-				`create table if not exists ${RoomName} (id text primary key not null, date text, sender text, receiver text, peer_key_hash text, server_key_hash text, encrypt_AES_Key text, encrypt_data blob);`,
-				(_, result) => {
-					console.log('테이블 생성 성공:', result);
-				},
-				(_, err) => {
-					console.log('테이블 생성 실패:', err);
-					return false; // 롤백
-				}
-			);
-
-			tx.executeSql(
-				`INSERT INTO ${RoomName} (id, date, sender, receiver, peer_key_hash, server_key_hash, encrypt_AES_Key, encrypt_data) VALUES (?, ?, ?, ?, ?, ?, ?, ?);`,
-				['1', new Date().toISOString(), '2', null, null, null, null, 'Hello!'],
-				(_, result) => {
-					console.log('데이터 삽입 성공:', result);
-				},
-				(_, err) => {
-					console.log('데이터 삽입 실패:', err);
-					return false; // 롤백
-				}
-			);
-
-			tx.executeSql(`SELECT * FROM ${RoomName};`, [], (_, { rows }) => {
-				console.log('전체 데이터:', rows);
-				for (let i = 0; i < rows.length; i++) {
-					console.log(`Row ${i + 1}:`, rows.item(i));
-				}
-			});
-		});
-
-		fetchMessages(); // 함수를 실행합니다.
-	}, []); // 컴포넌트가 마운트될 때 한 번만 실행됩니다.
+		Make_new_DB();
+	}, []);
 
 	useEffect(() => {
 		const timer = setTimeout(() => {
 			const newMessage: IMessage = {
-				_id: messages.length + 1,
 				text: '자동으로 받은 메시지입니다.',
 				createdAt: new Date(),
 				user: {
@@ -203,7 +181,9 @@ export default function ChatScreen() {
 		<GiftedChat
 			messages={messages}
 			onSend={(newMessages) => onSend(newMessages)}
-			user={{ _id: 1 }}
+			user={{
+				_id: 1,
+			}}
 		/>
 	);
 }
