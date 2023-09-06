06acd78d74690693337ed71735650e4a4447182c
이진형 <dlwlsgud2416@naver.com>
2023-09-06 05:57:17 +0000

    웹소켓 테스트 함

--- ExpoWebsocketExample/App.tsx
+++ ExpoWebsocketExample/App.tsx
@@ -1,17 +1,18 @@
 import React, { useEffect } from 'react';
-import { View, Text } from 'react-native';
-import io from 'socket.io-client';
+import { Text, View, Button } from 'react-native';
+import { io } from 'socket.io-client';
 
 export default function App() {
   useEffect(() => {
-    const socket = io('http://flaskwstest.run.goorm.site:5000');
+    const socket = io('http://3.36.111.111:55369');
     
     socket.on('connect', () => {
-      console.log('Connected to server');
+      console.log('connected to server');
+      socket.emit('message', 'Hello from client');
     });
 
     socket.on('message', (message) => {
-      console.log('New message:', message);
+      console.log(`Message from server: ${message}`);
     });
 
     return () => {
@@ -21,7 +22,7 @@ export default function App() {
 
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Check your console for WebSocket messages</Text>
+      <Text>WebSocket Example</Text>
     </View>
   );
 }
--- FlaskWS_for_Test/app.py
+++ FlaskWS_for_Test/app.py
@@ -1,7 +1,3 @@
-# app.py
-
-#python3 -m venv myenv
-#source myenv/bin/activate
 from flask import Flask, render_template
 from flask_socketio import SocketIO
 
@@ -10,12 +6,12 @@ socketio = SocketIO(app)
 
 @app.route("/")
 def main():
-    return render_template("index.html")
+    return "WebSocket Server is running"
 
 @socketio.on("message")
 def handle_message(msg):
-    print("Message: " + msg)
-    socketio.emit("message", msg)
+    print("Received message:", msg)
+    socketio.emit("message", "This is a message from the server")
 
 if __name__ == "__main__":
-    socketio.run(app, host='0.0.0.0', debug=True, port=5000)
+    socketio.run(app, host="0.0.0.0", port=5000)
