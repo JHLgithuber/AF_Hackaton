ec69338f0ba6e3039c8a0bf491e83d80373d4576
이진형 <dlwlsgud2416@naver.com>
2023-09-10 05:29:01 +0000

    GPT 클라이언트에서 쓸 암호화모듈 만듬

--- Client_GPT/HybridCryptoModuleForNodeJS.tsx
+++ Client_GPT/HybridCryptoModuleForNodeJS.tsx
@@ -3,30 +3,34 @@
 //https://www.npmjs.com/package/react-native-rsa-expo
 //https://www.npmjs.com/package/crypto-js
 import React, { useState, useEffect } from 'react';
-import * as SQLite from 'expo-sqlite';
-import * as Crypto from 'expo-crypto';
-import * as TaskManager from 'expo-task-manager';
-import * as BackgroundFetch from 'expo-background-fetch';
+//import * as SQLite from 'expo-sqlite';
+//import * as Crypto from 'expo-crypto';
+//import * as TaskManager from 'expo-task-manager';
+//import * as BackgroundFetch from 'expo-background-fetch';
 import { InteractionManager } from 'react-native';
 import { self } from 'react-native-workers';
-import * as SecureStore from 'expo-secure-store';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+//import * as SecureStore from 'expo-secure-store';
+i//mport AsyncStorage from '@react-native-async-storage/async-storage';
 import base64 from 'base-64';
 import CryptoJS from 'crypto-js';
-import Toast from 'react-native-root-toast';
-import {get_server_private_key} from './ConnectionModule';
+//import Toast from 'react-native-root-toast';
+import { get_server_private_key } from './ConnectionModule';
 
-const KeyPair_DB = SQLite.openDatabase('Encrypted_Chat_Data.db');
+const KeyStore_db = level('./KeyStore_PublicKey');
+const crypto = require('crypto');
+const sqlite3 = require('sqlite3').verbose();
+const level = require('level');
+
+const KeyPair_DB = new sqlite3.Database('Encrypted_Chat_Data.db');
 const RSAKey = require('react-native-rsa');
 const bits = 512; //안전한건 2048 이상
 const exponent = '65537'; // must be a string. This is hex string. decimal = 65537
 
-
 export async function Get_KeyStore_PrivateKey(authenticationPrompt) {
     try {
-        let KeyStore_PrivateKey = await SecureStore.getItemAsync('KeyStore_PrivateKey',{authenticationPrompt:authenticationPrompt});
+        let KeyStore_PrivateKey = await KeyStore_db.get('KeyStore_PrivateKey');
         if (KeyStore_PrivateKey) {
-			console.log('Get_KeyStore_PrivateKey', KeyStore_PrivateKey);
+            console.log('Get_KeyStore_PrivateKey', KeyStore_PrivateKey);
             return KeyStore_PrivateKey;
         } else {
             RSA_KeyPair_Maker();
@@ -38,9 +42,9 @@ export async function Get_KeyStore_PrivateKey(authenticationPrompt) {
 
 async function Get_KeyStore_PublicKey() {
     try {
-        const KeyStore_PublicKey = await AsyncStorage.getItem('KeyStore_PublicKey');
+        const KeyStore_PublicKey = await KeyStore_db.get('KeyStore_PublicKey');
         if (KeyStore_PublicKey !== null) {
-			console.log('Get_KeyStore_PublicKey', KeyStore_PublicKey);
+            console.log('Get_KeyStore_PublicKey', KeyStore_PublicKey);
             return KeyStore_PublicKey;
         }
     } catch (e) {
@@ -49,16 +53,16 @@ async function Get_KeyStore_PublicKey() {
 }
 
 async function Set_KeyStore_Key() {
-	 const rsa = new RSAKey();
-            await rsa.generate(bits, exponent);
-	console.log("Set_KeyStore_Key",rsa.getPublicString(),rsa.getPrivateString())
+    const rsa = new RSAKey();
+    await rsa.generate(bits, exponent);
+    console.log('Set_KeyStore_Key', rsa.getPublicString(), rsa.getPrivateString());
 
     try {
-        await AsyncStorage.setItem('KeyStore_PublicKey', rsa.getPublicString());
-        await SecureStore.setItemAsync('KeyStore_PrivateKey', rsa.getPrivateString(),{authenticationPrompt:"ㅄㅋㅋㅋㅋㅋ",requireAuthentication:true});
+        await KeyStore_db.put('KeyStore_PublicKey', rsa.getPublicString());
+        await KeyStore_db.put('KeyStore_PrivateKey', rsa.getPrivateString());
     } catch (e) {
         console.log('Set_KeyStore_Key', e);
-    }
+    }KeyStore_PublicKey_db
 }
 
 function Make_RSA_KeyTable() {
@@ -92,7 +96,7 @@ export function Remove_RSA_KeyTable() {
             (_, result) => {
                 console.log('Table Dropped:', result);
                 // 테이블 다시 생성
-                RSA_KeyPair_Maker()
+                RSA_KeyPair_Maker();
             },
             (_, err) => {
                 console.log('Drop Table Error:', err);
@@ -119,84 +123,77 @@ KeyPair_DB.transaction((tx) => {
 	});
 });*/
 
-export function RSA_KeyPair_Maker() {
+export async function RSA_KeyPair_Maker() {
     Make_RSA_KeyTable();
     Set_KeyStore_Key();
-    InteractionManager.runAfterInteractions(async () => {
-        const toast = Toast.show('RSA KeyPair 생성중...');
-        try {
-            console.log('Making_RSA_Key_Pair...');
-            var Start_MakingKey = new Date();
-            console.log('MakingKey Time:\t', Start_MakingKey);
+    try {
+        console.log('Making_RSA_Key_Pair...');
+        var Start_MakingKey = new Date();
+        console.log('MakingKey Time:\t', Start_MakingKey);
 
-            const rsa = new RSAKey();
-            await rsa.generate(bits, exponent);
-            const publicKey = rsa.getPublicString(); // return json encoded string
-            const privateKey = rsa.getPrivateString(); // return json encoded string
-            console.log('publicKey:\t', publicKey);
-            console.log('privateKey:\t', privateKey);
-            //console.log(typeof rsa);
+        const rsa = new RSAKey();
+        await rsa.generate(bits, exponent);
+        const publicKey = rsa.getPublicString(); // return json encoded string
+        const privateKey = rsa.getPrivateString(); // return json encoded string
+        console.log('publicKey:\t', publicKey);
+        console.log('privateKey:\t', privateKey);
+        //console.log(typeof rsa);
 
-            var End_MakingKey = new Date();
-            console.log('Required Time:\t', End_MakingKey - Start_MakingKey);
+        var End_MakingKey = new Date();
+        console.log('Required Time:\t', End_MakingKey - Start_MakingKey);
 
-            const publicKeyHash = await Crypto.digestStringAsync(
-                Crypto.CryptoDigestAlgorithm.SHA512,
-                publicKey
+        const publicKeyHash = CryptoJS.SHA512(publicKey).toString(CryptoJS.enc.Hex);
+
+        console.log('publicKeyHash:\t', publicKeyHash);
+
+        const rsa_key = new RSAKey();
+        console.log(await Get_KeyStore_PublicKey());
+        console.log(typeof (await Get_KeyStore_PublicKey()));
+        rsa_key.setPublicString(await Get_KeyStore_PublicKey());
+        console.log('rsa_key.setPublicString');
+        const randomBytes = crypto.randomBytes(32);
+        const AESKey = base64.encode(String.fromCharCode.apply(null, randomBytes));
+        const encrypted_privateKey = CryptoJS.AES.encrypt(privateKey, AESKey).toString();
+        const encrypted_AES_key = rsa_key.encrypt(AESKey);
+        console.log('encrypted_privateKey:\t', encrypted_AES_key);
+
+        KeyPair_DB.transaction((tx) => {
+            tx.executeSql(
+                'INSERT INTO KeyTable (public_key_hash, public_key, encrypted_private_key, encrypted_AES_key, generated_date, used_date) VALUES (?, ?, ?, ?, ?,?)',
+                [
+                    publicKeyHash,
+                    publicKey,
+                    encrypted_privateKey,
+                    encrypted_AES_key,
+                    new Date().toISOString(),
+                    null,
+                ],
+                (_, result) => {
+                    console.log('INSERT Key:', result);
+                },
+                (_, err) => {
+                    console.log('INSERT Key ERROR:', err);
+                    return false;
+                }
             );
-            console.log('publicKeyHash:\t', publicKeyHash);
-
-            const rsa_key = new RSAKey();
-            console.log(await Get_KeyStore_PublicKey());
-            console.log(typeof await Get_KeyStore_PublicKey());
-            rsa_key.setPublicString(await Get_KeyStore_PublicKey());
-            console.log('rsa_key.setPublicString');
-            const randomBytes = Crypto.getRandomValues(new Uint8Array(32));
-            const AESKey = base64.encode(String.fromCharCode.apply(null, randomBytes));
-            const encrypted_privateKey = CryptoJS.AES.encrypt(privateKey, AESKey).toString();
-            const encrypted_AES_key = rsa_key.encrypt(AESKey);
-            console.log('encrypted_privateKey:\t', encrypted_AES_key);
-
-            KeyPair_DB.transaction((tx) => {
-                tx.executeSql(
-                    'INSERT INTO KeyTable (public_key_hash, public_key, encrypted_private_key, encrypted_AES_key, generated_date, used_date) VALUES (?, ?, ?, ?, ?,?)',
-                    [
-                        publicKeyHash,
-                        publicKey,
-                        encrypted_privateKey,
-                        encrypted_AES_key,
-                        new Date().toISOString(),
-                        null,
-                    ],
-                    (_, result) => {
-                        console.log('INSERT Key:', result);
-                    },
-                    (_, err) => {
-                        console.log('INSERT Key ERROR:', err);
-                        return false;
-                    }
-                );
-            });
-            Toast.hide(toast);
-        } catch (error) {
-            Toast.hide(toast);
-            console.error('An error occurred:', error);
-        }
-    });
+        });
+    } catch (error) {
+        console.error('An error occurred:', error);
+    }
 }
 
 //상대의 공개키로 암호화
 export function Encryption(publicKey, serverKey, data) {
     try {
         const rsa = new RSAKey();
-		const rsa_server = new RSAKey();
-		console.log("publicKey",publicKey);
+        const rsa_server = new RSAKey();
+        console.log('publicKey', publicKey);
         rsa.setPublicString(publicKey);
-		console.log("serverKey",serverKey);
-		rsa_server.setPublicString(serverKey);
+        console.log('serverKey', serverKey);
+        rsa_server.setPublicString(serverKey);
 
         // AES 암호화 키 생성
-        const randomBytes = Crypto.getRandomValues(new Uint8Array(32));
+        const randomBytes = crypto.randomBytes(32);
         const AESKey = base64.encode(String.fromCharCode.apply(null, randomBytes));
         console.log('AESKey', AESKey);
 
@@ -207,10 +204,10 @@ export function Encryption(publicKey, serverKey, data) {
 
         const ciphertext = CryptoJS.AES.encrypt(data, AESKey).toString();
         console.log('ciphertext', ciphertext);
-		
+
         const encrypted_AESKey = rsa.encrypt(AESKey);
         console.log('encrypted_AESKey', encrypted_AESKey);
-		const server_encrypted_AESKey = rsa_server.encrypt(encrypted_AESKey);
+        const server_encrypted_AESKey = rsa_server.encrypt(encrypted_AESKey);
         console.log('server_encrypted_AESKey', server_encrypted_AESKey);
 
         return { ciphertext, server_encrypted_AESKey };
@@ -221,12 +218,18 @@ export function Encryption(publicKey, serverKey, data) {
 }
 
 //자신의 비밀키로 복호화
-export async function Decryption(public_key_hash, server_key_hash, encrypt_AES_Key, ciphertext, preReady_private_key) {
+export async function Decryption(
+    public_key_hash,
+    server_key_hash,
+    encrypt_AES_Key,
+    ciphertext,
+    preReady_private_key
+) {
     try {
         let encrypted_privateKey = null;
         let encrypted_AES_key_for_key = null;
-        		
-		console.log('encrypted_AES_key is exist?', encrypt_AES_Key);
+
+        console.log('encrypted_AES_key is exist?', encrypt_AES_Key);
 
         await new Promise((resolve, reject) => {
             KeyPair_DB.transaction((tx) => {
@@ -257,38 +260,36 @@ export async function Decryption(public_key_hash, server_key_hash, encrypt_AES_K
             console.log('encrypted_privateKey is null');
             return null;
         }
-		
-		
-		const rsa_key = new RSAKey();
-		if(preReady_private_key){
-			rsa_key.setPrivateString(preReady_private_key);
-		}else{
-			rsa_key.setPrivateString(await Get_KeyStore_PrivateKey("For Decryption"));
-		}
-        
-		
-		//서버에서 비밀키를 가져와서 AES키를 1차 복호화
-		const rsa_server = new RSAKey();
-		console.log(await get_server_private_key(server_key_hash));
-		rsa_server.setPrivateString(await get_server_private_key(server_key_hash));
-		console.log("encrypt_AES_Key",encrypt_AES_Key);
-		const server_decrypt_AES_key=await rsa_server.decrypt(encrypt_AES_Key);
-		console.log("server_decrypt_AES_key",server_decrypt_AES_key);
-		
-		//암호화되어있는 로컬 비밀키를 복호화
+
+        const rsa_key = new RSAKey();
+        if (preReady_private_key) {
+            rsa_key.setPrivateString(preReady_private_key);
+        } else {
+            rsa_key.setPrivateString(await Get_KeyStore_PrivateKey('For Decryption'));
+        }
+
+        //서버에서 비밀키를 가져와서 AES키를 1차 복호화
+        const rsa_server = new RSAKey();
+        console.log(await get_server_private_key(server_key_hash));
+        rsa_server.setPrivateString(await get_server_private_key(server_key_hash));
+        console.log('encrypt_AES_Key', encrypt_AES_Key);
+        const server_decrypt_AES_key = await rsa_server.decrypt(encrypt_AES_Key);
+        console.log('server_decrypt_AES_key', server_decrypt_AES_key);
+
+        //암호화되어있는 로컬 비밀키를 복호화
         const AES_Key = await rsa_key.decrypt(encrypted_AES_key_for_key);
         console.log('AES_KEY=rsa_key.decrypt', AES_Key);
         var privateKey_bytes = await CryptoJS.AES.decrypt(encrypted_privateKey, AES_Key);
         var privateKey = await privateKey_bytes.toString(CryptoJS.enc.Utf8);
         console.log('privateKey', privateKey);
-		
-		//복호화된 로컬 비밀키로 AES키를 최종 복호화
-		const rsa = new RSAKey();
+
+        //복호화된 로컬 비밀키로 AES키를 최종 복호화
+        const rsa = new RSAKey();
         await rsa.setPrivateString(privateKey);
         const Data_AES_Key = await rsa.decrypt(server_decrypt_AES_key);
-		console.log("Data_AES_Key",Data_AES_Key);
-		
-		//복호화된 AES키로 데이터 복호화
+        console.log('Data_AES_Key', Data_AES_Key);
+
+        //복호화된 AES키로 데이터 복호화
         var bytes = await CryptoJS.AES.decrypt(ciphertext, Data_AES_Key);
         var originalData = await bytes.toString(CryptoJS.enc.Utf8);
         console.log('originalData', originalData);
